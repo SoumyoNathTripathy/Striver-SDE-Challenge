807fd6ce76c3c3d49349339be0876aaa71b0ed53
SOUMYO NATH TRIPATHY <85414445+SoumyoNathTripathy@users.noreply.github.com>
2023-06-09 16:22:08 +0530

    Create count inversion.java

---
+++ count inversion.java
@@ -0,0 +1,24 @@
+/* 
+    Time Complexity : O(N ^ 2)
+    Space Complexity : O(1)
+
+    Where 'N' is the total number of elements in the array/list.
+*/
+
+public class Solution {
+    public static long getInversions(long[] arr, int n) {
+        // Variable to store the total inversions in the array.
+        long totalInversions = 0;
+
+        // Check for each element whether any smaller element is present on right side.
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    totalInversions += 1;
+                }
+
+            }
+        }
+        return totalInversions;
+    }
+}
